44205504d547bfda3ec54cc05f692db10da6c272
Luciano Dias <luciano.dias@softdesign.com.br>
2022-12-28 18:53:21 -0300

    teste 1

--- index.js
+++ index.js
@@ -12,32 +12,32 @@ const actions = {
 (async () => {
   const {
     t: ticket = "",
-    o: baseKbPath = `C:\\Projetos\\Genexus\\Models\\Develop`,
-    d: destKbPath = `C:\\Projetos\\Genexus\\Models\\KBPoc2022_1.1`,
-    a: action = actions.import,
+    o: baseKbPath = `Develop`,
+    d: destKbPath = `KBPoc2022_1.1`,
+    a: action = actions.export,
   } = argv;
 
   try {
     console.log({ ticket, baseKbPath, destKbPath, action });
-    // const xpzName = `export_${ticket}.xpz`;
-    // switch (action) {
-    //   case actions.export: {
-    //     const filename = `export_${ticket}.msbuild`;
-    //     const commitsFile = "./commit_list_poc2022_develop.xml";
-    //     const exportCommitXml = await exportXml(xpzName, ticket, commitsFile);
-    //     const xmlBuild = generate(baseKbPath, targetNames.ExportCommit, exportCommitXml);
-    //     await fs.writeFile(`./${filename}`, xmlBuild);
-    //     console.info(`Arquivo ${filename} gerado.`);
-    //     break;
-    //   }
-    //   case actions.import:
-    //     const filename = `import_${ticket}.msbuild`;
-    //     const importXml = importCommitXml(xpzName);
-    //     const xmlBuild = generate(destKbPath, targetNames.ImportCommit, importXml);
-    //     await fs.writeFile(`./${filename}`, xmlBuild);
-    //     console.info(`Arquivo ${filename} gerado.`);
-    //     break;
-    // }
+    const xpzName = `export_${ticket}.xpz`;
+    switch (action) {
+      case actions.export: {
+        const filename = `export_${ticket}.msbuild`;
+        const commitsFile = "./commits.xml";
+        const exportCommitXml = await exportXml(xpzName, ticket, commitsFile);
+        const xmlBuild = generate(baseKbPath, targetNames.ExportCommit, exportCommitXml);
+        await fs.writeFile(`./${filename}`, xmlBuild);
+        console.info(`Arquivo ${filename} gerado.`);
+        break;
+      }
+      case actions.import:
+        const filename = `import_${ticket}.msbuild`;
+        const importXml = importCommitXml(xpzName);
+        const xmlBuild = generate(destKbPath, targetNames.ImportCommit, importXml);
+        await fs.writeFile(`./${filename}`, xmlBuild);
+        console.info(`Arquivo ${filename} gerado.`);
+        break;
+    }
   } catch (error) {
     console.info(error.message);
   }
